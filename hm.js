969451641088d84803fb2aa8a8b51626df88c84d
Aleks <33761093+AleksK88@users.noreply.github.com>
2017-12-13 22:27:24 +0200

    Add files via upload

---
+++ hm.js
@@ -0,0 +1,6 @@
+var name = prompt("Enter your name:", "Aleks");
+var time = prompt("Enter time:", "13:00");
+var place = prompt("Where are you?", "in Kiev");
+var event = prompt("Enter the event", "dinner");
+var result = name + " is going to" + " " + event + " " + "at" + " " + time + " " + place;
+alert(result);
